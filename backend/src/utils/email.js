3bdb347805e2f7b7012edeb4c9c926dab38a8112
sreenilay07 <sreenilayreddy@gmail.com>
2026-05-23 20:48:07 +0530

    Change email transport to secure connection

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -4,7 +4,7 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false, // true for 465, false for other ports
+    secure: true, // true for 465, false for other ports
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
